e6b27b20acfa5622a5404ac8ba7fe920eb2687ab
mlbtvps <119069067+mlbtvps@users.noreply.github.com>
2023-04-22 20:01:31 +0630

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -308,7 +308,7 @@ def get_readable_message():
         bmsg += f"\n<b>• Free Disk:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
         bmsg += f"\n<b>• Total Downloading Speed:</b> {get_readable_file_size(dl_speed)}/s"
         bmsg += f"\n<b>• Total Uploading Speed:</b> {get_readable_file_size(up_speed)}/s"
-        bmsg += f"<b>• ‎‎<a href='https://t.me/leechanimegp'>Powered By Source Please</a></b>\n"
+        bmsg += f"<b>  ‎‎<a href='https://t.me/leechanimegp'>Powered By Source Please</a></b>\n"
 
         buttons = ButtonMaker()
         buttons.sbutton("Refresh", "status refresh")
